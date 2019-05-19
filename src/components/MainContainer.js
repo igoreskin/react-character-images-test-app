6baad117f4136fff6b63f8be1f776ab282c7cfc4
Igor Eskin <eskinhd@gmail.com>
2019-05-18 21:11:48 -0400

    Initial commit

---
+++ src/components/MainContainer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const MainContainer = () => {
+
+  const characters = [
+    {"image": "images/luke.jpg", "name": "Luke Skywalker"},
+    { "image": "images/darth.jpg", "name": "Darth Vader" },
+    { "image": "images/obi.png", "name": "Ob-wan Kenobi" },
+    { "image": "images/r2d2.jpg", "name": "R2-D2" }
+  ]
+
+  return (
+    <div>MainContainer</div>
+  )
+
+}
+
+export default MainContainer;
